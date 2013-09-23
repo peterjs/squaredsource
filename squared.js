c132f8be2160792d091da389339f05d24848ed55
Peter Somlo <peter.somlo@gmail.com>
2013-09-23 15:39:29 +0200

    Rewritten with Bacon.js

--- squared.js
+++ squared.js
@@ -18,80 +18,78 @@ window.onload = function(e) {
         var c = document.getElementById('c');
         c.textContent = "hello";
 
-        console.log('here');
-
         gw.log(path, function(err, logs) {
             if (!err) {
-//                console.log(logs);
-//                //use (var x in a) rather than (x in a) - don't want to create a global
-//                //All objects in JS are associative
-//                var dates = [];
-//                for (var m in logs) {
-//                    dates.push(logs[m].authorDate);
-//                }
-//                //filter undefined
-//                dates = dates.filter(function(d){ return d;});
-//                //months are 0-11
-//                var days = 365;
-//                dates = dates.map(function(date) {
-//                    var today = new Date();
-//                    today = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
-//                    return ((today - date)/(24*60*60*1000));
-//                }).map(function(diff) {
-//                        //commits made today are (-1.0,0.0]
-//                        return Math.floor(days - diff - 1);
-//                    });
-//
-//                //bucket dates
-//
-////        var zeros = new Array(days).
-//                var zeros = Array.apply(null, new Array(days)).map(Number.prototype.valueOf,0);
-//                var buckets = dates.reduce(function(bucks, day) {
-//                    if (day < days) {
-//                        bucks[day] = (bucks[day] || 0) + 1;
-//                    }
-//                    return bucks;
-//                }, zeros);
-//
-//                var calendar1 = document.getElementById('calendar');
-////        var calendar = document.createElement('g');
-//
-//                var minBuckets = Math.min.apply(null, buckets);
-//                var maxBuckets = Math.max.apply(null, buckets);
-//
-//                var NS="http://www.w3.org/2000/svg";
-//                var calendar = document.createElementNS(NS, 'g');
-//                calendar.setAttribute('transform','translate(20,20)');
-//                calendar1.appendChild(calendar);
-//                var lastDayInColumn = new Date().getUTCDay();
-//
-//                var currentDay = days-1;
-//                for (var column = Math.ceil(days/7); column >= 0; column--) {
-//                    for (var row = lastDayInColumn; row >= 0 && currentDay >= 0; row--) {
-//                        var x = column*13;
-//                        var y = row*13;
-//                        var  bucket = buckets[currentDay];
-//                        var transform = document.createElementNS(NS, 'g');
-//                        transform.setAttribute('transform','translate('+x+','+y+')');
-//
-//                        var rect = document.createElementNS(NS,'rect');
-//
-//                        rect.setAttribute('class','day');
-//                        rect.setAttribute('y','0');
-//                        rect.setAttribute('width','11');
-//                        rect.setAttribute('height','11');
-//                        var color = (!bucket)?'000000':'555555';
-//                        color = getColor(bucket,minBuckets,maxBuckets,colormap());
-//                        rect.setAttribute('style','fill: '+color+';');
-//                        transform.appendChild(rect);
-//                        calendar.appendChild(transform);
-//                        console.log(bucket || '0');
-//                        currentDay = currentDay - 1;
-//                    }
-//                    lastDayInColumn = 6;
-//                }
-//
-//                c.textContent = buckets;
+                console.log(logs);
+                //use (var x in a) rather than (x in a) - don't want to create a global
+                //All objects in JS are associative
+                var dates = [];
+                for (var m in logs) {
+                    dates.push(logs[m].authorDate);
+                }
+                //filter undefined
+                dates = dates.filter(function(d){ return d;});
+                //months are 0-11
+                var days = 365;
+                dates = dates.map(function(date) {
+                    var today = new Date();
+                    today = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
+                    return ((today - date)/(24*60*60*1000));
+                }).map(function(diff) {
+                        //commits made today are (-1.0,0.0]
+                        return Math.floor(days - diff - 1);
+                    });
+
+                //bucket dates
+
+//        var zeros = new Array(days).
+                var zeros = Array.apply(null, new Array(days)).map(Number.prototype.valueOf,0);
+                var buckets = dates.reduce(function(bucks, day) {
+                    if (day < days) {
+                        bucks[day] = (bucks[day] || 0) + 1;
+                    }
+                    return bucks;
+                }, zeros);
+
+                var calendar1 = document.getElementById('calendar');
+//        var calendar = document.createElement('g');
+
+                var minBuckets = Math.min.apply(null, buckets);
+                var maxBuckets = Math.max.apply(null, buckets);
+
+                var NS="http://www.w3.org/2000/svg";
+                var calendar = document.createElementNS(NS, 'g');
+                calendar.setAttribute('transform','translate(20,20)');
+                calendar1.appendChild(calendar);
+                var lastDayInColumn = new Date().getUTCDay();
+
+                var currentDay = days-1;
+                for (var column = Math.ceil(days/7); column >= 0; column--) {
+                    for (var row = lastDayInColumn; row >= 0 && currentDay >= 0; row--) {
+                        var x = column*13;
+                        var y = row*13;
+                        var  bucket = buckets[currentDay];
+                        var transform = document.createElementNS(NS, 'g');
+                        transform.setAttribute('transform','translate('+x+','+y+')');
+
+                        var rect = document.createElementNS(NS,'rect');
+
+                        rect.setAttribute('class','day');
+                        rect.setAttribute('y','0');
+                        rect.setAttribute('width','11');
+                        rect.setAttribute('height','11');
+                        var color = (!bucket)?'000000':'555555';
+                        color = getColor(bucket,minBuckets,maxBuckets,colormap());
+                        rect.setAttribute('style','fill: '+color+';');
+                        transform.appendChild(rect);
+                        calendar.appendChild(transform);
+                        console.log(bucket || '0');
+                        currentDay = currentDay - 1;
+                    }
+                    lastDayInColumn = 6;
+                }
+
+                c.textContent = buckets;
             }
         });
     };
@@ -116,42 +114,6 @@ window.onload = function(e) {
         });
     };
 
-//    var repoPath = function() {
-//        var repos = document.getElementById('repos');
-//        repos.addEventListener('keyup', function() {
-//            console.log(repos.value);
-//            validGitPath(repos.value);
-//        });
-//        repos.addEventListener('keydown', function() {
-//            var key = event.keyCode || event.charCode;
-//            if (key == 8 || key == 46) {
-////                validGitPath(repos.value);
-//            }
-//            console.log(repos.value);
-//            validGitPath(repos.value);
-//        });
-//    };
-//    repoPath();
-
-//    var userFilter = function() {
-//        var user = document.getElementById('user');
-//        user.addEventListener('keyup', function() {
-////            console.log(user.value);
-//            lastUser = user;
-//            makeCalendar(path, lastUser);
-//        });
-//        repos.addEventListener('keydown', function() {
-//            var key = event.keyCode || event.charCode;
-//            if (key == 8 || key == 46) {
-////                validGitPath(repos.value);
-//            }
-////            console.log(user.value);
-//            lastUser = user;
-//            makeCalendar(path, lastUser);
-//        });
-//    };
-//    userFilter();
-
     var colormap = function() {
         //colorbrewer2.org
         var colors = [[241,238,246], [189,201,225], [116,169,207], [42,140,190], [4,90,141]];
@@ -172,8 +134,6 @@ window.onload = function(e) {
 
 //    require('baconjs');
     var baconButton = document.getElementById('baconbutton');
-//    var baconEventStream = baconButton.asEventStream('click');
-//    baconEventStream.onValue(function() {alert('hello frp!')});
     var baconEventStream = Bacon.fromEventTarget(baconButton, "click");
     baconEventStream.onValue(function() { alert("Hello Bacon!") });
 
@@ -190,30 +150,21 @@ window.onload = function(e) {
     var user = textFieldValue(userTextField).toProperty('me').skipDuplicates();
     var repo = textFieldValue(repoTextField).toProperty('.').skipDuplicates();
 
-    function cal(user, path) {
-        console.log(path + ' ' + user);
-        return path + ' ' + user;
-    }
-
-//    var c = user.toProperty('').combine(repo.toProperty(''),cal);
-//    c.log();
-
-    function f(user, repo) {
-        console.log(repo + ' ' + user);
-        makeCalendar(repo, user);
-    }
-//    Bacon.onValues(user, repo, f);
+    //repo add / - on different platforms as last character
 
     var validFoldersRepos = repo.flatMapLatest(function(path){
        return Bacon.fromCallback(fs.exists, path);
     });
 
-    validFoldersRepos.log();
+    //a.sampledBy(b, function);
+    //Like combine, but only outputs a new value on a new value to the b stream.
+    var ok = repo.sampledBy(validFoldersRepos.filter(function(a) {return a;}), function(a, b) {
+//       return 'repo: ' + a + ' ' + b;
+        return a;
+    });
 
-    function toLogsStream(path) {
-        return Bacon.fromNodeCallback(gw.log, path);
-    }
-
-//    var logsStream = repo.flatMapLatest(toLogsStream);
-//    logsStream.log();
+    ok.onValue(function(a) {
+        console.log(a);
+        makeCalendar(a);
+    });
 };
