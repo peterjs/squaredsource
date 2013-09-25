c33999c0f2c023e341b97bd6dea85f74efd2cfef
Peter Somlo <peter.somlo@gmail.com>
2013-09-26 00:09:57 +0200

    Basis for datamodel represented by FRP

--- squared.js
+++ squared.js
@@ -116,6 +116,16 @@ window.onload = function(e) {
         return colormap[Math.round(val/(max-min)*colorMapMax)];
     };
 
+    function SquaredModel () {
+        //public in
+        this.repoAdded = new Bacon.Bus();
+
+        function addRepo (newRepo) {return function (repos) {return repos.concat([newRepo])}};
+        var modifications = this.repoAdded.map(addRepo);
+        //public out
+        this.allRepos = modifications.scan([], function (repos, modification) {return modification(repos)});
+    }
+
 //    require('baconjs');
     function textFieldValue(textField) {
         var textFieldEvenetStream = Bacon.fromEventTarget(textField, 'keyup');
