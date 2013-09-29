03f7b2c078176b32fd36681d093d29df9e9c5063
Peter Somlo <peter.somlo@gmail.com>
2013-09-30 00:56:28 +0200

    Partially put into views

--- squared.js
+++ squared.js
@@ -12,109 +12,7 @@ window.onload = function(e) {
     var gitExeFilename = '/opt/local/bin/git';
     console.log('found git: ' + gitExeFilename);
 
-    var gw = require('./lib/gitWrapper');
-
-    var makeCalendar = function(path, user, element) {
-        var c = document.getElementById('c');
-        c.textContent = "hello";
-
-        gw.log(path, function(err, logs) {
-            if (!err) {
-//                console.log(logs);
-//                console.log('user ' + user);
-                logs = logs.filter(function(log) {return (log.hasOwnProperty('author') && (log.author.indexOf(user) === 0));});
-                //use (var x in a) rather than (x in a) - don't want to create a global
-                var dates = logs.map(function(log) {return log.authorDate;});
-                //filter undefined
-                dates = dates.filter(function(d){ return d;});
-                //months are 0-11
-                var days = 365;
-                dates = dates.map(function(date) {
-                    var today = new Date();
-                    today = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
-                    return ((today - date)/(24*60*60*1000));
-                }).map(function(diff) {
-                        //commits made today are (-1.0,0.0]
-                        return Math.floor(days - diff - 1);
-                    });
-
-                //bucket dates
-
-//        var zeros = new Array(days).
-                var zeros = Array.apply(null, new Array(days)).map(Number.prototype.valueOf,0);
-                var buckets = dates.reduce(function(bucks, day) {
-                    if (day < days) {
-                        bucks[day] = (bucks[day] || 0) + 1;
-                    }
-                    return bucks;
-                }, zeros);
-
-                var NS="http://www.w3.org/2000/svg";
-
-                var calendarSvgElem = document.createElementNS(NS,'svg');
-                calendarSvgElem.setAttribute('width', '900');
-                calendarSvgElem.setAttribute('height', '110');
-
-                var minBuckets = Math.min.apply(null, buckets);
-                var maxBuckets = Math.max.apply(null, buckets);
-
-                var calendar = document.createElementNS(NS, 'g');
-                calendarSvgElem.appendChild(calendar);
-                element.appendChild(calendarSvgElem);
-
-                calendar.setAttribute('transform','translate(20,20)');
-
-                var lastDayInColumn = new Date().getUTCDay();
-                var currentDay = days-1;
-                for (var column = Math.ceil(days/7); column >= 0; column--) {
-                    for (var row = lastDayInColumn; row >= 0 && currentDay >= 0; row--) {
-                        var x = column*13;
-                        var y = row*13;
-                        var  bucket = buckets[currentDay];
-                        var transform = document.createElementNS(NS, 'g');
-                        transform.setAttribute('transform','translate('+x+','+y+')');
-
-                        var rect = document.createElementNS(NS,'rect');
-
-                        rect.setAttribute('class','day');
-                        rect.setAttribute('y','0');
-                        rect.setAttribute('width','11');
-                        rect.setAttribute('height','11');
-                        var color = (!bucket)?'000000':'555555';
-                        color = getColor(bucket,minBuckets,maxBuckets,colormap());
-                        rect.setAttribute('style','fill: '+color+';');
-                        transform.appendChild(rect);
-                        calendar.appendChild(transform);
-//                        console.log(bucket || '0');
-                        currentDay = currentDay - 1;
-                    }
-                    lastDayInColumn = 6;
-                }
-
-                c.textContent = buckets;
-            }
-        });
-    };
-
     var fs = require('fs');
-
-    var colormap = function() {
-        //colorbrewer2.org
-        var colors = [[241,238,246], [189,201,225], [116,169,207], [42,140,190], [4,90,141]];
-        var hexColors = colors.map(function (c) {
-            return c.reduce(function (hexColor, val) {
-                var hexVal = val.toString(16);
-                if (val < 16) {hexVal = '0' + hexVal;}
-                return hexColor + hexVal;
-            }, '#');
-        });
-        return hexColors;
-    };
-
-    var getColor = function(val, min, max, colormap) {
-        var colorMapMax = colormap.length-1;
-        return colormap[Math.round(val/(max-min)*colorMapMax)];
-    };
 
 //    require('baconjs');
     function textFieldValue(textField) {
@@ -138,7 +36,7 @@ window.onload = function(e) {
 
     var t = function(x) {return x;};
     var toBool = function(x) {if (x) {return true;} else {return false;}};
-
+    var gw = require('./lib/gitWrapper');
     var isValidFoldersReposGetStatus = repo.sampledBy(isValidFoldersReposFsExists.filter(t)).flatMapLatest(function(path){
         return Bacon.fromNodeCallback(gw.getStatus, path);
     }).map(toBool).mapError(function(a){return false;}).toProperty(false);
@@ -171,28 +69,6 @@ window.onload = function(e) {
         return oldRepos.indexOf(newRepo)>=0?oldRepos:oldRepos.concat([newRepo]);
     });
 
-    addedReposProp.combine(user, function(p, u) {
-        return {path: p, user:u};
-    }).onValue(function(pu) {
-            var repoArray = pu.path;
-
-            var repoListElem = document.getElementById('repo_list');
-            while (repoListElem.firstChild) {
-                repoListElem.removeChild(repoListElem.firstChild);
-            }
-            var listElem = document.createElement('ul');
-            repoListElem.appendChild(listElem);
-            repoArray.forEach(function(repo) {
-                var repoElem = document.createElement('li');
-                repoElem.textContent = repo;
-                repoListElem.appendChild(repoElem);
-
-                var repoCalendarElem = document.createElement('div');
-                makeCalendar(repo, pu.user, repoCalendarElem);
-                repoListElem.appendChild(repoCalendarElem);
-            });
-        });
-
     function SquaredModel() {
         //public in
         this.repoAdded = new Bacon.Bus();
@@ -204,12 +80,152 @@ window.onload = function(e) {
 //        this.allRepos.log();
     }
 
-    function RepositoryView() {
+    function RepositoryView(repo) {
+        var colormap = function() {
+            //colorbrewer2.org
+            var colors = [[241,238,246], [189,201,225], [116,169,207], [42,140,190], [4,90,141]];
+            var hexColors = colors.map(function (c) {
+                return c.reduce(function (hexColor, val) {
+                    var hexVal = val.toString(16);
+                    if (val < 16) {hexVal = '0' + hexVal;}
+                    return hexColor + hexVal;
+                }, '#');
+            });
+            return hexColors;
+        };
+
+        var getColor = function(val, min, max, colormap) {
+            var colorMapMax = colormap.length-1;
+            return colormap[Math.round(val/(max-min)*colorMapMax)];
+        };
+
+
+
+        this.makeCalendar = function(path, user, element) {
+            var c = document.getElementById('c');
+            c.textContent = "hello";
+
+            gw.log(path, function(err, logs) {
+                if (!err) {
+    //                console.log(logs);
+    //                console.log('user ' + user);
+                    logs = logs.filter(function(log) {return (log.hasOwnProperty('author') && (log.author.indexOf(user) === 0));});
+                    //use (var x in a) rather than (x in a) - don't want to create a global
+                    var dates = logs.map(function(log) {return log.authorDate;});
+                    //filter undefined
+                    dates = dates.filter(function(d){ return d;});
+                    //months are 0-11
+                    var days = 365;
+                    dates = dates.map(function(date) {
+                        var today = new Date();
+                        today = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
+                        return ((today - date)/(24*60*60*1000));
+                    }).map(function(diff) {
+                            //commits made today are (-1.0,0.0]
+                            return Math.floor(days - diff - 1);
+                        });
+
+                    //bucket dates
+
+    //        var zeros = new Array(days).
+                    var zeros = Array.apply(null, new Array(days)).map(Number.prototype.valueOf,0);
+                    var buckets = dates.reduce(function(bucks, day) {
+                        if (day < days) {
+                            bucks[day] = (bucks[day] || 0) + 1;
+                        }
+                        return bucks;
+                    }, zeros);
+
+                    var NS="http://www.w3.org/2000/svg";
+
+                    var calendarSvgElem = document.createElementNS(NS,'svg');
+                    calendarSvgElem.setAttribute('width', '900');
+                    calendarSvgElem.setAttribute('height', '110');
+
+                    var minBuckets = Math.min.apply(null, buckets);
+                    var maxBuckets = Math.max.apply(null, buckets);
+
+                    var calendar = document.createElementNS(NS, 'g');
+                    calendarSvgElem.appendChild(calendar);
+                    element.appendChild(calendarSvgElem);
+
+                    calendar.setAttribute('transform','translate(20,20)');
+
+                    var lastDayInColumn = new Date().getUTCDay();
+                    var currentDay = days-1;
+                    for (var column = Math.ceil(days/7); column >= 0; column--) {
+                        for (var row = lastDayInColumn; row >= 0 && currentDay >= 0; row--) {
+                            var x = column*13;
+                            var y = row*13;
+                            var  bucket = buckets[currentDay];
+                            var transform = document.createElementNS(NS, 'g');
+                            transform.setAttribute('transform','translate('+x+','+y+')');
+
+                            var rect = document.createElementNS(NS,'rect');
+
+                            rect.setAttribute('class','day');
+                            rect.setAttribute('y','0');
+                            rect.setAttribute('width','11');
+                            rect.setAttribute('height','11');
+                            var color = (!bucket)?'000000':'555555';
+                            color = getColor(bucket,minBuckets,maxBuckets,colormap());
+                            rect.setAttribute('style','fill: '+color+';');
+                            transform.appendChild(rect);
+                            calendar.appendChild(transform);
+    //                        console.log(bucket || '0');
+                            currentDay = currentDay - 1;
+                        }
+                        lastDayInColumn = 6;
+                    }
+
+                    c.textContent = buckets;
+                }
+            });
+        };
 
     }
 
-    function RepositoryListView() {
+    function RepositoryListView(listElement, model) {
+//        function render(repos) {
+//            listElement.children().remove();
+//            repos.forEach(addRepo);
+//        }
 
+        function addRepo(repo) {
+            console.log('adding repo ' + repo);
+
+        }
+
+        model.repoAdded.onValue(function(repo){
+                addRepo(repo);
+            }
+        );
+
+        addedReposProp.combine(user, function(p, u) {
+            return {path: p, user:u};
+        }).onValue(function(pu) {
+                var repoArray = pu.path;
+
+                var repoListElem = document.getElementById('repo_list');
+                while (repoListElem.firstChild) {
+                    repoListElem.removeChild(repoListElem.firstChild);
+                }
+                var listElem = document.createElement('ul');
+                repoListElem.appendChild(listElem);
+                repoArray.forEach(function(repo) {
+                    var repoElem = document.createElement('li');
+                    repoElem.textContent = repo;
+                    repoListElem.appendChild(repoElem);
+
+                    var repoCalendarElem = document.createElement('div');
+                    var repoView = new RepositoryView(repo);
+                    repoView.makeCalendar(repo, pu.user, repoCalendarElem);
+                    repoListElem.appendChild(repoCalendarElem);
+                });
+            });
+
+//        var repaint = model.repoDeleted;
+//        repaint.onValue(render);
     }
 
     function NewRepositoryView() {
@@ -217,13 +233,7 @@ window.onload = function(e) {
     }
 
     function UserFilterView() {
-        
-    }
 
-    function SquaredView(model) {
-        model.allRepos.onValue(function(val) {
-            console.log('view got ' + val);
-        });
     }
 
 //    model.repoAdded.push('test string');
@@ -231,7 +241,7 @@ window.onload = function(e) {
 
     function SquaredApp() {
         var model = new SquaredModel();
-        var view = new SquaredView(model);
+        var view = new RepositoryListView('',model);
         model.repoAdded.plug(repoListStream);
     }
 
