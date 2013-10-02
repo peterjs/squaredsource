8e12a9f169a866605215162e5504e4d61f483999
Peter Somlo <peter.somlo@gmail.com>
2013-10-02 18:02:40 +0200

    Added support for building releases with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+//    grunt.initConfig({
+//        nodeunit: {
+//            files: ['test/**/*_test.js']
+//        },
+//        jshint: {
+//            options: {
+//                jshintrc: '.jshintrc'
+//            },
+//            gruntfile: {
+//                src: 'Gruntfile.js'
+//            },
+//            lib: {
+//                src: ['lib/**/*.js']
+//            },
+//            test: {
+//                src: ['test/**/*.js']
+//            },
+//        },
+//        watch: {
+//            gruntfile: {
+//                files: '<%= jshint.gruntfile.src %>',
+//                tasks: ['jshint:gruntfile']
+//            },
+//            lib: {
+//                files: '<%= jshint.lib.src %>',
+//                tasks: ['jshint:lib', 'nodeunit']
+//            },
+//            test: {
+//                files: '<%= jshint.test.src %>',
+//                tasks: ['jshint:test', 'nodeunit']
+//            },
+//        },
+//    });
+
+    grunt.initConfig({
+      nodewebkit: {
+        options: {
+            build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+            mac: true, // We want to build it for mac
+            win: true, // We want to build it for win
+            linux32: false, // We don't need linux32
+            linux64: false // We don't need linux64
+        },
+          // TODO only include the modules defined in package.json
+          src: ['./*', './lib/*', './img/*', './node_modules/**/**/*']
+      }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+    // Default task.
+//    grunt.registerTask('default', ['jshint', 'nodeunit']);
+
+    grunt.registerTask('default', 'nodewebkit');
+};
