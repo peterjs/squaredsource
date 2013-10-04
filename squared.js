0aade149640ee0b2bc613605e6d2ba318aa82768
Peter Somlo <peter.somlo@gmail.com>
2013-10-04 21:18:29 +0200

    Fixed calendar update with flatMap

--- squared.js
+++ squared.js
@@ -64,92 +64,94 @@ window.onload = function(e) {
 
         var element = document.createElement('div');
 
-        model.userProp.combine(Bacon.once(), function(a,b){return a;}).onValue(function(user) {
-            while (element.firstChild) {
-                element.removeChild(element.firstChild);
-            }
-            var c = document.getElementById('c');
-            c.textContent = "hello";
-
-            gw.log(path, function(err, logs) {
-                if (!err) {
-                    //                console.log(logs);
-                    //                console.log('user ' + user);
-                    logs = logs.filter(function(log) {return (log.hasOwnProperty('author') && (log.author.indexOf(user) === 0));});
-                    //use (var x in a) rather than (x in a) - don't want to create a global
-                    var dates = logs.map(function(log) {return log.authorDate;});
-                    //filter undefined
-                    dates = dates.filter(function(d){ return d;});
-                    //months are 0-11
-                    var days = 365;
-                    dates = dates.map(function(date) {
-                        var today = new Date();
-                        today = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
-                        return ((today - date)/(24*60*60*1000));
-                    }).map(function(diff) {
-                            //commits made today are (-1.0,0.0]
-                            return Math.floor(days - diff - 1);
-                        });
-
-                    //bucket dates
-
-                    //        var zeros = new Array(days).
-                    var zeros = Array.apply(null, new Array(days)).map(Number.prototype.valueOf,0);
-                    var buckets = dates.reduce(function(bucks, day) {
-                        if (day < days) {
-                            bucks[day] = (bucks[day] || 0) + 1;
-                        }
-                        return bucks;
-                    }, zeros);
-
-                    var NS="http://www.w3.org/2000/svg";
-                    var calendarSvgElem = document.createElementNS(NS,'svg');
-                    element.appendChild(calendarSvgElem);
-
-                    calendarSvgElem.setAttribute('width', '784');
-                    calendarSvgElem.setAttribute('height', '110');
-
-                    var minBuckets = Math.min.apply(null, buckets);
-                    var maxBuckets = Math.max.apply(null, buckets);
-
-                    var calendar = document.createElementNS(NS, 'g');
-                    calendarSvgElem.appendChild(calendar);
-                    element.appendChild(calendarSvgElem);
-
-                    calendar.setAttribute('transform','translate(20,20)');
-
-                    var lastDayInColumn = new Date().getUTCDay();
-                    var currentDay = days-1;
-                    for (var column = Math.ceil(days/7); column >= 0; column--) {
-                        for (var row = lastDayInColumn; row >= 0 && currentDay >= 0; row--) {
-                            var x = column*13;
-                            var y = row*13;
-                            var  bucket = buckets[currentDay];
-                            var transform = document.createElementNS(NS, 'g');
-                            transform.setAttribute('transform','translate('+x+','+y+')');
-
-                            var rect = document.createElementNS(NS,'rect');
-
-                            rect.setAttribute('class','day');
-                            rect.setAttribute('y','0');
-                            rect.setAttribute('width','11');
-                            rect.setAttribute('height','11');
-                            var color = (!bucket)?'000000':'555555';
-                            color = getColor(bucket,minBuckets,maxBuckets,colormap());
-                            rect.setAttribute('style','fill: '+color+';');
-                            transform.appendChild(rect);
-                            calendar.appendChild(transform);
-                            //                        console.log(bucket || '0');
-                            currentDay = currentDay - 1;
-                        }
-                        lastDayInColumn = 6;
-                    }
-
-                    c.textContent = buckets;
+        var aaa = model.userProp.combine(Bacon.once(), function(a,b){return a;}).flatMapLatest(function(user) {
+            return Bacon.fromNodeCallback(gw.log, path).map(function(logs) {
+                while (element.firstChild) {
+                    element.removeChild(element.firstChild);
                 }
+                var c = document.getElementById('c');
+                c.textContent = "hello";
+//console.log('heheheheheheheeh dasdoadosadoasodisa');
+//            gw.log(path, function(err, logs) {
+//                if (!err) {
+                //                console.log(logs);
+                //                console.log('user ' + user);
+                logs = logs.filter(function(log) {return (log.hasOwnProperty('author') && (log.author.indexOf(user) === 0));});
+                //use (var x in a) rather than (x in a) - don't want to create a global
+                var dates = logs.map(function(log) {return log.authorDate;});
+                //filter undefined
+                dates = dates.filter(function(d){ return d;});
+                //months are 0-11
+                var days = 365;
+                dates = dates.map(function(date) {
+                    var today = new Date();
+                    today = new Date(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
+                    return ((today - date)/(24*60*60*1000));
+                }).map(function(diff) {
+                        //commits made today are (-1.0,0.0]
+                        return Math.floor(days - diff - 1);
+                    });
+
+                //bucket dates
+
+                //        var zeros = new Array(days).
+                var zeros = Array.apply(null, new Array(days)).map(Number.prototype.valueOf,0);
+                var buckets = dates.reduce(function(bucks, day) {
+                    if (day < days) {
+                        bucks[day] = (bucks[day] || 0) + 1;
+                    }
+                    return bucks;
+                }, zeros);
+
+                var NS="http://www.w3.org/2000/svg";
+                var calendarSvgElem = document.createElementNS(NS,'svg');
+                element.appendChild(calendarSvgElem);
+
+                calendarSvgElem.setAttribute('width', '784');
+                calendarSvgElem.setAttribute('height', '110');
+
+                var minBuckets = Math.min.apply(null, buckets);
+                var maxBuckets = Math.max.apply(null, buckets);
+
+                var calendar = document.createElementNS(NS, 'g');
+                calendarSvgElem.appendChild(calendar);
+                element.appendChild(calendarSvgElem);
+
+                calendar.setAttribute('transform','translate(20,20)');
+
+                var lastDayInColumn = new Date().getUTCDay();
+                var currentDay = days-1;
+                for (var column = Math.ceil(days/7); column >= 0; column--) {
+                    for (var row = lastDayInColumn; row >= 0 && currentDay >= 0; row--) {
+                        var x = column*13;
+                        var y = row*13;
+                        var  bucket = buckets[currentDay];
+                        var transform = document.createElementNS(NS, 'g');
+                        transform.setAttribute('transform','translate('+x+','+y+')');
+
+                        var rect = document.createElementNS(NS,'rect');
+
+                        rect.setAttribute('class','day');
+                        rect.setAttribute('y','0');
+                        rect.setAttribute('width','11');
+                        rect.setAttribute('height','11');
+                        var color = (!bucket)?'000000':'555555';
+                        color = getColor(bucket,minBuckets,maxBuckets,colormap());
+                        rect.setAttribute('style','fill: '+color+';');
+                        transform.appendChild(rect);
+                        calendar.appendChild(transform);
+                        //                        console.log(bucket || '0');
+                        currentDay = currentDay - 1;
+                    }
+                    lastDayInColumn = 6;
+                }
+
+                c.textContent = buckets;
+//                }
             });
         });
         this.element = element;
+        aaa.log();
     }
 
     function RepositoryListView(repoListElem, model) {
