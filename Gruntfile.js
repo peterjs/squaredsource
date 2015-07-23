2138b8eba04e40efe13ff0a766e94bdb33781532
Peter Somlo <peter.somlo@gmail.com>
2015-07-23 15:42:40 +0200

    Build release for Linux x64

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
             mac: true, // We want to build it for mac
             win: true, // We want to build it for win
             linux32: false, // We don't need linux32
-            linux64: false // We don't need linux64
+            linux64: true // We don't need linux64
         },
           // TODO only include the modules defined in package.json
           src: ['./*', './lib/*', './img/*', './node_modules/**/**/*']
